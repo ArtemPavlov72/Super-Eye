1ed3f5d0ce3d489e3beaa55e3c2a98dff46fcc03
Artem Pavlov <pavlov.tema@gmail.com>
2021-12-01 22:04:37 +0500

    Initial Commit

---
+++ Super Eye/Super_EyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_EyeApp.swift
+//  Super Eye
+//
+//  Created by iMac on 01.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Super_EyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
