900c405e837481af22386fedfb6b941a036277c7
Artem Pavlov <pavlov.tema@gmail.com>
2021-12-01 22:14:50 +0500

    1. TabView added on ContentView. 2. MainView added.

---
+++ Super Eye/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Super Eye
+//
+//  Created by iMac on 01.12.2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
