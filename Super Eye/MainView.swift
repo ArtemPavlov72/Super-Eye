415a85fa24c534d8a8560dcc0065e282c825bdbb
Artem Pavlov <pavlov.tema@gmail.com>
2021-12-03 21:54:27 +0500

    Button "Start exercises" added on MainView. (No action)

--- Super Eye/MainView.swift
+++ Super Eye/MainView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text("Main Page")
+        Button(action: {}) {
+            Text("Начать зарядку")
+                .font(.title)
+        }
     }
 }
 
