bfd0886b8da6f8280fd0460b9fe046419652ea6c
Artem Pavlov <pavlov.tema@gmail.com>
2021-12-07 21:37:13 +0500

    TimerCount V1

--- Super Eye/TimeCounter.swift
+++ Super Eye/TimeCounter.swift
@@ -7,14 +7,67 @@
 
 import Foundation
 import Combine
+import SwiftUI
+
+enum CurrentExercise {
+    case relaxation, blinking, main
+}
 
 class TimeCounter: ObservableObject {
     
-let objectWillChange = PassthroughSubject<TimeCounter, Never>()
+    let objectWillChange = PassthroughSubject<TimeCounter, Never>()
+    
+    var currentExercise = CurrentExercise.relaxation
+    
     var counter = 60
     var timer: Timer?
-    var titleOfExercise = "Управжение 1"
+    var titleOfExercise = "Приготовьтесь"
     
+    func startTimer() {
+        if counter > 0 {
+            timer = Timer.scheduledTimer(
+                timeInterval: 1,
+                target: self,
+                selector: #selector(updateCounter),
+                userInfo: nil,
+                repeats: true
+            )
+        }
+     //   titleOfExerciseDidChanged()
+    }
     
+    @objc private func updateCounter() {
+        if counter > 0 {
+            counter -= 1
+        } else {
+            killTimer()
+            titleOfExerciseDidChanged()
+        }
+        objectWillChange.send(self)
+        
+    }
+    
+    private func killTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    private func titleOfExerciseDidChanged() {
+        
+        switch currentExercise {
+        case .relaxation:
+            counter = 7
+            titleOfExercise = "Расслабление"
+            currentExercise = .blinking
+        case .blinking:
+            counter = 5
+            titleOfExercise = "Быстро моргаем"
+            currentExercise = .main
+        case .main:
+            counter = 10
+            titleOfExercise = "Основное упражнение"
+        }
+        objectWillChange.send(self)
+    }
     
 }
