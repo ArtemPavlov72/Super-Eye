7ca74b5f7f7217448e553ac4dcaadb723e495bde
Artem Pavlov <pavlov.tema@gmail.com>
2021-12-19 17:20:01 +0500

    1. Model "Exercise" created. 2. Model "Exercise" updated with names and descriptions of exercises. (version 1, without EyeViews)

--- Super Eye/Models/Exercise.swift
+++ Super Eye/Models/Exercise.swift
@@ -9,4 +9,25 @@ import SwiftUI
 
 struct Exercise: Identifiable {
     let id: Int
+    let name: String
+    let description: String
+    
+    static func getExercisesList() -> [Exercise] {
+        
+        var exercises: [Exercise] = []
+        
+        let names = DataManager.shared.nameOfExercises
+        let descriptions = DataManager.shared.descriptionOfExercises
+        
+        for index in 0..<names.count {
+            let exercise = Exercise(
+                id: index + 1,
+                name: names[index],
+                description: descriptions[index]
+            )
+            exercises.append(exercise)
+        }
+        return exercises
+    }
+    
 }
