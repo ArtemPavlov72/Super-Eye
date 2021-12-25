ecf65961619b4c032b19a87fb32e327ba5908c78
Artem Pavlov <pavlov.tema@gmail.com>
2021-12-25 12:43:33 +0500

    titleOfExerciseDidChanged() in TimeCounter updated.

--- Super Eye/TimeCounter.swift
+++ Super Eye/TimeCounter.swift
@@ -71,71 +71,71 @@ class TimeCounter: ObservableObject {
     private func titleOfExerciseDidChanged() {
         
         if playStop {
-        switch currentExercise {
-        case .relaxation:
-            counter = Exercises.relaxation.exerciseData.duration
-            titleOfExercise = Exercises.relaxation.exerciseData.name
-            currentExercise = .blinking
-        case .blinking:
-            counter = Exercises.blinking.exerciseData.duration
-            titleOfExercise = Exercises.blinking.exerciseData.name
-            currentExercise = .upDown
-        case .upDown:
-            counter = Exercises.upDown.exerciseData.duration
-            titleOfExercise = Exercises.upDown.exerciseData.name
-            currentExercise = .leftRight
-        case .leftRight:
-            counter = Exercises.leftRight.exerciseData.duration
-            titleOfExercise = Exercises.leftRight.exerciseData.name
-            currentExercise = .diagonalHL
-        case .diagonalHL:
-            counter = Exercises.diagonalHL.exerciseData.duration
-            titleOfExercise = Exercises.diagonalHL.exerciseData.name
-            currentExercise = .diagonalLH
-        case .diagonalLH:
-            counter = Exercises.diagonalLH.exerciseData.duration
-            titleOfExercise = Exercises.diagonalLH.exerciseData.name
-            currentExercise = .squareClockwise
-        case .squareClockwise:
-            counter = Exercises.squareClockwise.exerciseData.duration
-            titleOfExercise = Exercises.squareClockwise.exerciseData.name
-            currentExercise = .squareCounterClockwise
-        case .squareCounterClockwise:
-            counter = Exercises.squareCounterClockwise.exerciseData.duration
-            titleOfExercise = Exercises.squareCounterClockwise.exerciseData.name
-            currentExercise = .circleClockwise
-        case .circleClockwise:
-            counter = Exercises.circleClockwise.exerciseData.duration
-            titleOfExercise = Exercises.circleClockwise.exerciseData.name
-            currentExercise = .circleConterClockwise
-        case .circleConterClockwise:
-            counter = Exercises.circleConterClockwise.exerciseData.duration
-            titleOfExercise = Exercises.circleConterClockwise.exerciseData.name
-            currentExercise = .slide
-        case .slide:
-            counter = Exercises.slide.exerciseData.duration
-            titleOfExercise = Exercises.slide.exerciseData.name
-            currentExercise = .snake
-        case .snake:
-            counter = Exercises.snake.exerciseData.duration
-            titleOfExercise = Exercises.snake.exerciseData.name
-            currentExercise = .distance
-        case .distance:
-            counter = Exercises.distance.exerciseData.duration
-            titleOfExercise = Exercises.distance.exerciseData.name
-            playStop.toggle()
-        }
+            switch currentExercise {
+            case .relaxation:
+                counter = Exercises.relaxation.exerciseData.duration
+                titleOfExercise = Exercises.relaxation.exerciseData.name
+                currentExercise = .blinking
+            case .blinking:
+                counter = Exercises.blinking.exerciseData.duration
+                titleOfExercise = Exercises.blinking.exerciseData.name
+                currentExercise = .upDown
+            case .upDown:
+                counter = Exercises.upDown.exerciseData.duration
+                titleOfExercise = Exercises.upDown.exerciseData.name
+                currentExercise = .leftRight
+            case .leftRight:
+                counter = Exercises.leftRight.exerciseData.duration
+                titleOfExercise = Exercises.leftRight.exerciseData.name
+                currentExercise = .diagonalHL
+            case .diagonalHL:
+                counter = Exercises.diagonalHL.exerciseData.duration
+                titleOfExercise = Exercises.diagonalHL.exerciseData.name
+                currentExercise = .diagonalLH
+            case .diagonalLH:
+                counter = Exercises.diagonalLH.exerciseData.duration
+                titleOfExercise = Exercises.diagonalLH.exerciseData.name
+                currentExercise = .squareClockwise
+            case .squareClockwise:
+                counter = Exercises.squareClockwise.exerciseData.duration
+                titleOfExercise = Exercises.squareClockwise.exerciseData.name
+                currentExercise = .squareCounterClockwise
+            case .squareCounterClockwise:
+                counter = Exercises.squareCounterClockwise.exerciseData.duration
+                titleOfExercise = Exercises.squareCounterClockwise.exerciseData.name
+                currentExercise = .circleClockwise
+            case .circleClockwise:
+                counter = Exercises.circleClockwise.exerciseData.duration
+                titleOfExercise = Exercises.circleClockwise.exerciseData.name
+                currentExercise = .circleConterClockwise
+            case .circleConterClockwise:
+                counter = Exercises.circleConterClockwise.exerciseData.duration
+                titleOfExercise = Exercises.circleConterClockwise.exerciseData.name
+                currentExercise = .slide
+            case .slide:
+                counter = Exercises.slide.exerciseData.duration
+                titleOfExercise = Exercises.slide.exerciseData.name
+                currentExercise = .snake
+            case .snake:
+                counter = Exercises.snake.exerciseData.duration
+                titleOfExercise = Exercises.snake.exerciseData.name
+                currentExercise = .distance
+            case .distance:
+                counter = Exercises.distance.exerciseData.duration
+                titleOfExercise = Exercises.distance.exerciseData.name
+                playStop.toggle()
+            }
         } else {
             killTimer()
+            playStop.toggle()
+            isPaused = true
             titleOfExercise = Exercises.relaxation.exerciseData.name
             counter = Exercises.relaxation.exerciseData.duration
-            currentExercise = .relaxation
-            
-            
+            currentExercise = .blinking
         }
-        
         objectWillChange.send(self)
     }
+    
     // смена картинки кнопки
     private func buttonDidTapped() {
         if buttonImage == "pause.fill" {
